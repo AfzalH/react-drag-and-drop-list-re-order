9aca9a598963b5a786131343bf23920d4f12c63b
Afzal Hossain <afzal.csedu@gmail.com>
2020-05-03 13:35:29 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import React from "react";
 import List from "./data";
 import { ListContainer, ListItem } from "./styles";
 import { DragHandle } from "./partials/DragHandle";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const App = () => {
   const list = List.getList();
   return (
     <div className="App">
-      <ListContainer>
-        <h1>The List</h1>
-        {list.map((item, i) => (
-          <ListItem>
-            <DragHandle />
-            <span>{item.title}</span>
-          </ListItem>
-        ))}
-      </ListContainer>
+      <DragDropContext
+        onDragEnd={(param) => {
+          const srcI = param.source.index;
+          const desI = param.destination?.index;
+          if (desI) {
+            list.splice(desI, 0, list.splice(srcI, 1)[0]);
+            List.saveList(list);
+          }
+        }}
+      >
+        <ListContainer>
+          <h1>The List</h1>
+          <Droppable droppableId="droppable-1">
+            {(provided, _) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {list.map((item, i) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={"draggable-" + item.id}
+                    index={i}
+                  >
+                    {(provided, snapshot) => (
+                      <ListItem
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        style={{
+                          ...provided.draggableProps.style,
+                          boxShadow: snapshot.isDragging
+                            ? "0 0 .4rem #666"
+                            : "none",
+                        }}
+                      >
+                        <DragHandle {...provided.dragHandleProps} />
+                        <span>{item.title}</span>
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </ListContainer>
+      </DragDropContext>
     </div>
   );
 };
